74ca3245a226a875c372a696cca96a2f9633fcdb
stanleykoz <56391633+stanleykoz@users.noreply.github.com>
2020-05-31 16:59:43 +0200

    Revert "Instancing done"
    This reverts commit 7c2071676b8a55ba4054ad27b4ca8dd147e7647a, reversing
    changes made to 4c735a8e0aba2b1467046c4022468fd05cfdcd90.

--- PBL/Clumsy/src/RenderEngine/Mesh.cpp
+++ PBL/Clumsy/src/RenderEngine/Mesh.cpp
@@ -58,7 +58,7 @@ namespace Clumsy
             {
                 number = std::to_string(specular_nr++);
             }
-			//std::cout << "Textury ziomeczka" <<textures.size() << std::endl;
+
             glBindTexture(GL_TEXTURE_2D, textures[i].id);
             glUniform1i(glGetUniformLocation(shaders_program, ("material." + name + number).c_str()), i);
 
@@ -80,53 +80,6 @@ namespace Clumsy
             glBindTexture(GL_TEXTURE_2D, 0);
         }
     }
-
-	void Mesh::DrawInstanced(unsigned int shaders_program, unsigned int amount)
-	{
-		int diffuse_nr = 1;
-		int specular_nr = 1;
-
-		for (int i = 0; i < textures.size(); i++)
-		{
-			glActiveTexture(GL_TEXTURE0 + i);
-
-			std::string number;
-			std::string name = textures[i].type;
-			if (name == "texture_diffuse")
-			{
-				number = std::to_string(diffuse_nr++);
-			}
-			else if (name == "texture_specular")
-			{
-				number = std::to_string(specular_nr++);
-			}
-			//std::cout << "Textury ziomeczka" <<textures.size() << std::endl;
-			glBindTexture(GL_TEXTURE_2D, textures[i].id);
-			glUniform1i(glGetUniformLocation(shaders_program, ("material." + name + number).c_str()), i);
-
-			//cout << "added in shader : " << ("material." + name + number).c_str() << endl;
-		}
-
-		//glUniform1f(glGetUniformLocation(shaders_program, "material.shininess"), 32.0f);
-
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		//glLineWidth(2);
-		//Draw
-		
-			glBindVertexArray(VAO);
-			glDrawElementsInstanced(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0, amount);
-			glBindVertexArray(0);
-		
-		//glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-		//glBindVertexArray(0);
-
-		for (int i = 0; i < textures.size(); i++)
-		{
-			glActiveTexture(GL_TEXTURE0 + i);
-			glBindTexture(GL_TEXTURE_2D, 0);
-		}
-	}
 
     void Mesh::setupMesh()
     {
