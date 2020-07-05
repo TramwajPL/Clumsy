b48abe182d2e1efc80f163a605391a3b85b0e1e1
jan-koz <56391633+stanleykoz@users.noreply.github.com>
2020-07-05 14:08:31 -0500

    Cieniasy vol 2

--- PBL/Clumsy/src/RenderEngine/RenderEngine.cpp
+++ PBL/Clumsy/src/RenderEngine/RenderEngine.cpp
@@ -211,7 +211,7 @@ namespace Clumsy
 
 		float near_plane = 1.0f, far_plane = 7.5f;
 		lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
-		lightView = glm::lookAt(glm::vec3(20.0f, 40.0f, -20.0f), glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+		lightView = glm::lookAt(glm::vec3(-2.0f, 4.0f, -1.0f), glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
 		lightSpaceMatrix = lightProjection * lightView;
 
 		// render scene from light's point of view
